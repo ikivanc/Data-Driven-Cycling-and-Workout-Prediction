64282e888a7ce1cfb7e9775524f6b2164d3f7c2d
Ibrahim KIVANC <ikivanc@hotmail.com>
2021-04-18 04:02:41 +0300

    webapi endpoint is updated and thumbnailcard response is added

--- bot/Bots/CyclingBot.cs
+++ bot/Bots/CyclingBot.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 //
 
+using CyclingPrediction.Models;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
 using Newtonsoft.Json;
@@ -20,17 +21,14 @@ namespace CyclingPrediction.Bots
         {
             dynamic value = turnContext.Activity.Value;
 
-            if (value == null)
-            {
+            if (value == null){
                 string adaptiveCardPath = Path.Combine(".", "Resources", "WorkoutTemplate.json");
                 var cardAttachment = CreateAdaptiveCardAttachment(adaptiveCardPath);
                 await turnContext.SendActivityAsync(MessageFactory.Attachment(cardAttachment), cancellationToken);
             }
-            else
-            {
-
+            else{
                 // Test Print for Teams output
-                await turnContext.SendActivityAsync(MessageFactory.Text(value.ToString(), value.ToString()), cancellationToken);
+                //await turnContext.SendActivityAsync(MessageFactory.Text(value.ToString(), value.ToString()), cancellationToken);
 
                 // Retrieve the data from fields
                 string cardCity = value.cityInput;
@@ -38,44 +36,71 @@ namespace CyclingPrediction.Bots
                 string cardTime = value.timeInput;
 
                 // Test Print for Teams output
-                await turnContext.SendActivityAsync(MessageFactory.Text(cardCity+ cardDate + cardTime, cardCity + cardDate + cardTime), cancellationToken);
+                //await turnContext.SendActivityAsync(MessageFactory.Text(cardCity+ cardDate + cardTime, cardCity + cardDate + cardTime), cancellationToken);
 
-                string result = await PredictWorkout(cardCity, cardDate, cardTime);
-
-                //Post the API response to bot again
-                await turnContext.SendActivityAsync(MessageFactory.Text(result, result), cancellationToken);
+                // TODO
+                // Control inputs incase MSTeams mobile or other apps send empty inputs.
+                if(!String.IsNullOrEmpty(cardCity) && !String.IsNullOrEmpty(cardDate) && !String.IsNullOrEmpty(cardTime))
+                {
+                    WorkoutPrediction result = await PredictWorkout(cardCity, cardDate, cardTime);
+                    
+                    if (result.Error == null){
+                        Attachment attachment = GetThumbnailCard(result);
+                        //Post the API response to bot again
+                        await turnContext.SendActivityAsync(MessageFactory.Attachment(attachment), cancellationToken);
+                    }
+                    else{
+                        string errorMessage = "An error occured: " + result.Error;
+                        await turnContext.SendActivityAsync(MessageFactory.Text(errorMessage, errorMessage), cancellationToken);
+                    }
+                }
+                else{
+                    string errorMessage = "Invalid Input, Please make sure, you have correct inputs";
+                    await turnContext.SendActivityAsync(MessageFactory.Text(errorMessage, errorMessage), cancellationToken);
+                }
             }
         }
 
-        async private Task<String> PredictWorkout(string wCity, string wDate, string wTime)
+        private static Attachment GetThumbnailCard(WorkoutPrediction wprediction)
         {
-            try
+            var thumbnailCard = new ThumbnailCard
             {
-                using (HttpClient client = new HttpClient())
-                {
+                Title = String.Format("{0}, mininum {1}km", wprediction.Ride, wprediction.Distance),
+                Subtitle = wprediction.Weather,
+                Text = String.Format("Weather details: Temperature: {0}C, Wind: {1}", wprediction.Temp, wprediction.Wind),
+                Images = new List<CardImage> { new CardImage(wprediction.WeatherIcon) }
+            };
+
+            return thumbnailCard.ToAttachment();
+        }
+
+        async private Task<WorkoutPrediction> PredictWorkout(string wCity, string wDate, string wTime)
+        {
+            try{
+                using (HttpClient client = new HttpClient()){
                     //Assuming that the api takes the user message as a query paramater
                     string RequestURI = String.Format("https://datadrivencycling.azurewebsites.net/predict?city={0}&date={1}&time={2}",wCity,wDate,wTime);
                     HttpResponseMessage responsemMsg = await client.GetAsync(RequestURI);
-                    string apiResponse = "";
-                    if (responsemMsg.IsSuccessStatusCode)
-                    {
-                        apiResponse = await responsemMsg.Content.ReadAsStringAsync();
-                    }
+                    WorkoutPrediction predictionResult = new WorkoutPrediction();
+                    if (responsemMsg.IsSuccessStatusCode){
+                        string apiResponse = await responsemMsg.Content.ReadAsStringAsync();
 
-                    return apiResponse;
+                        predictionResult = JsonConvert.DeserializeObject<WorkoutPrediction>(apiResponse);
+                    }
+                    return predictionResult;
                 }
             }
-            catch (Exception ex)
-            {
-                return ex.ToString();
+            catch (Exception ex){
+                WorkoutPrediction predictionResult = new WorkoutPrediction();
+                predictionResult.Error = ex.ToString();
+                return predictionResult;
             }
         }
 
         private static Attachment CreateAdaptiveCardAttachment(string filePath)
         {
             var adaptiveCardJson = File.ReadAllText(filePath);
-            var adaptiveCardAttachment = new Attachment()
-            {
+            var adaptiveCardAttachment = new Attachment(){
                 ContentType = "application/vnd.microsoft.card.adaptive",
                 Content = JsonConvert.DeserializeObject(adaptiveCardJson),
             };
@@ -85,10 +110,8 @@ namespace CyclingPrediction.Bots
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
             var welcomeText = "Welcome to your personal Cycling Prediction. Please submit for your workout prediction!";
-            foreach (var member in membersAdded)
-            {
-                if (member.Id != turnContext.Activity.Recipient.Id)
-                {
+            foreach (var member in membersAdded){
+                if (member.Id != turnContext.Activity.Recipient.Id){
                     await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
                 }
             }
---
+++ bot/Models/WorkoutPrediction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CyclingPrediction.Models
+{
+    public class WorkoutPrediction
+    {
+        public int Distance { get; set; }
+        public string Ride { get; set; }
+        public int Temp { get; set; }
+        public string Weather { get; set; }
+        public string WeatherIcon { get; set; }
+        public int Wind { get; set; }
+        public string Error { get; set; }
+    }
+}
