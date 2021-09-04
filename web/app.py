d7dcd20bec1843f22b062ddc794dba23f824cc20
Ibrahim KIVANC <ikivanc@hotmail.com>
2021-09-04 23:51:36 +0300

    refactor code for pep8 style

--- web/app.py
+++ web/app.py
@@ -1,34 +1,43 @@
-import numpy as np
-import pandas as pd
-import pickle
 from utils import *
 from myconfig import *
 
 # Fast API libraries
 from fastapi import FastAPI
 import uvicorn
-from pydantic import BaseModel
 
 app = FastAPI()
+
 
 @app.get('/')
 def home():
     return "Cycling Prediction API"
 
+
 # http://127.0.0.1:8000/predict?city=Istanbul&date=2021-04-19&time=14:00
 @app.get("/predict")
-def predict( city: str, date: str, time: str ):
+def predict(city: str, date: str, time: str):
     try:
         # get parameters
-        workout_temp, workout_wind , workout_weatherdesc, workout_weathericon, result_ridetype, result_distance = predict_workout(api_key,city,date,time)
+        (workout_temp,
+         workout_wind,
+         workout_weatherdesc,
+         workout_weathericon,
+         result_ridetype,
+         result_distance) = predict_workout(api_key, city, date, time)
 
         # Parse result
         ride_type = "Outdoor Ride" if result_ridetype[0] == 0 else "Virtual Ride"
         distance = int(result_distance[0])
 
-        return {"temp": workout_temp, "wind": workout_wind , "weather": workout_weatherdesc,"weathericon": workout_weathericon,"ride": ride_type, "distance": distance }
+        return {"temp": workout_temp,
+                "wind": workout_wind,
+                "weather": workout_weatherdesc,
+                "weathericon": workout_weathericon,
+                "ride": ride_type,
+                "distance": distance}
     except Exception as ex:
         return str(ex)
+
 
 if __name__ == '__main__':
     print("main started")
--- web/utils.py
+++ web/utils.py
@@ -1,30 +1,36 @@
-from datetime import datetime, timedelta, time
-import requests, pickle, gzip , shutil
+import requests
+import pickle
 from sklearn import preprocessing
+from datetime import datetime, time
 
-# 7 - Predict Workout Model
-def get_weather(api_key,city,date,hour):
-    
+
+def get_weather(api_key, city, date, hour):
+    """
+    Predict weather with City name, date and hour of the day
+    """
+
     # Use different endpoints if it's an old weather data or future forecast
     if type(date) == str:
         date = datetime.fromisoformat(date)
-        
-    if date>datetime.now():
+
+    # Detect if it's a weather forecast or or old weather data
+    if date > datetime.now():
         endpoint = f"http://api.worldweatheronline.com/premium/v1/weather.ashx?key={api_key}&q={city}&date={date}&format=json"
     else:
         endpoint = f"http://api.worldweatheronline.com/premium/v1/past-weather.ashx?key={api_key}&q={city}&date={date}&format=json"
-    
+
     response = requests.get(endpoint)
-    api_results=response.json()
+    api_results = response.json()
 
-    ## TODO
-    ## Exception handling for API daily limit:
-    ## {'data': {'error': [{'msg': 'API key has reached calls per day allowed limit.'}]}}
+    # TODO
+    # Exception handling for API daily limit:
+    # {'data': {'error': [{'msg': 'API key has reached calls per day allowed limit.'}]}}
 
-    # Weather data is splitted into 3hour slices, in below code finding hour slot for weather
-    hour_slice = int(hour/3) 
+    # Weather data is splitted into 3hour slices,
+    # in below code finding hour slot for weather
+    hour_slice = int(hour/3)
 
-    # Selecting weather hour slice 
+    # Selecting weather hour slice
     temp = api_results['data']['weather'][0]['hourly'][hour_slice]
 
     # Return weather details
@@ -36,44 +42,50 @@ def get_weather(api_key,city,date,hour):
     return int(temp_c), int(windspeed_miles), weather_desc, weather_icon
 
 
-import os
-
-# 8 - Predict Workout
-def predict_workout(api_key,city,wdate,wtime):
+def predict_workout(api_key, city, wdate, wtime):
+    """
+    Predict weather with city name, date and time of the day
+    """
     wtime = time.fromisoformat(wtime)
-    temp_data = get_weather(api_key,city,wdate,wtime.hour)
-    workout_temp, workout_wind , workout_weatherdesc, workout_weathericon = temp_data
-    
+    temp_data = get_weather(api_key, city, wdate, wtime.hour)
+    workout_temp, workout_wind, workout_weatherdesc, workout_weathericon = temp_data
+
     # weather description
     le = preprocessing.LabelEncoder()
     le.fit(['Clear' 'Cloudy' 'Heavy rain' 'Light drizzle' 'Light rain'
-            'Light rain shower' 'Moderate rain' 'Moderate rain at times' 'Overcast'
-            'Partly cloudy' 'Patchy light rain with thunder' 'Patchy light snow'
-            'Patchy rain possible' 'Sunny'])
+            'Light rain shower' 'Moderate rain' 'Moderate rain at times'
+            'Overcast' 'Partly cloudy' 'Patchy light rain with thunder'
+            'Patchy light snow' 'Patchy rain possible' 'Sunny'])
 
     try:
         workout_weather = int(le.transform([workout_weatherdesc])[0])
-    except:
+    except Exception as ex:
         workout_weather = 0
-    
+
     wdate = datetime.fromisoformat(wdate)
     workout_hour = wtime.hour
     workout_dayofweek = wdate.isoweekday()
     workout_isweekend = int(workout_dayofweek // 6 == 1)
-    
+
     # Load ride type model from file
     ridetype_model_file = "./model/ridetype_model.pkl"
 
     with open(ridetype_model_file, 'rb') as file:
         ridetype_model = pickle.load(file)
-      
-    result_ridetype = ridetype_model.predict([[workout_hour,workout_dayofweek,workout_isweekend,workout_temp,workout_wind,workout_weather]])
+
+    result_ridetype = ridetype_model.predict(
+        [[workout_hour, workout_dayofweek, workout_isweekend,
+          workout_temp, workout_wind, workout_weather]])
     print("Result type prediction=%s" % result_ridetype)
-    
+
     # Load distance model from file
     distance_model_file = "./model/distance_model.pkl"
     with open(distance_model_file, 'rb') as file:
         distance_model = pickle.load(file)
-    
-    result = distance_model.predict([[workout_hour,workout_dayofweek,workout_isweekend,workout_temp,workout_wind,workout_weather]])
-    return workout_temp, workout_wind , workout_weatherdesc, workout_weathericon, result_ridetype, result
+
+    result = distance_model.predict(
+        [[workout_hour, workout_dayofweek, workout_isweekend,
+          workout_temp, workout_wind, workout_weather]])
+
+    return (workout_temp, workout_wind, workout_weatherdesc,
+            workout_weathericon, result_ridetype, result)
